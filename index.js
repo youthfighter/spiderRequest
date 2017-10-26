547711fd631e92c434099cea809d83351a7b45b2
李林军 <lin>
2017-10-26 09:51:50 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Export spiderRequest (with )
+ */
+
+exports = module.exports = require('./lib/spiderRequest');
+
+/*
+  Export the version
+*/
+
+exports.version = require('./package.json').version;
